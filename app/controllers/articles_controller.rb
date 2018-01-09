92b68afd4118d1d3b2bca27fd7578c75af15e46b
Coder Mind <codermind88@gmail.com>
2018-01-09 09:56:25 +0000

    adds bootstrap styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.user = User.first
     if @article.save
-      flash[:notice] = "Article was successfully created!"
+      flash[:success] = "Article was successfully created!"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
   
   def destroy
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:success] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
   
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article updated"
+      flash[:success] = "Article updated"
       redirect_to article_path(@article)
     else
       render 'edit'
